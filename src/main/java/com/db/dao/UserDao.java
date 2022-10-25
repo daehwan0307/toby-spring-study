de20ae6b4e1b9d7853ca43ff2b04142300aa81f9
daehwan0307 <eoghks960307@naver.com>
2022-10-25 13:24:16 +0900

    Factory 적용

--- src/main/java/com/db/dao/UserDao.java
+++ src/main/java/com/db/dao/UserDao.java
@@ -3,18 +3,17 @@ package com.db.dao;
 import com.db.domain.User;
 
 import java.sql.*;
-import java.util.Map;
 
 public class UserDao {
     private ConnectionMaker connectionMaker;
-    public UserDao(){
-        connectionMaker = new ConnectionMaker();
+    public UserDao(ConnectionMaker connectionMaker){
+        this.connectionMaker = connectionMaker;
     }
     public void add(User user) {
 
         try {
             // DB접속 (ex sql workbeanch실행)
-            Connection c = connectionMaker.getConnection();
+            Connection c = connectionMaker.makeConnection();
 
             // Query문 작성
             PreparedStatement pstmt = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
@@ -37,7 +36,7 @@ public class UserDao {
 
         try {
             // DB접속 (ex sql workbeanch실행)
-            Connection c = connectionMaker.getConnection();
+            Connection c = connectionMaker.makeConnection();
             // Query문 작성
             PreparedStatement pstmt = c.prepareStatement("SELECT * FROM users WHERE id = ?");
             pstmt.setString(1, id);
@@ -60,7 +59,7 @@ public class UserDao {
     }
 
     public static void main(String[] args) {
-        UserDao userDao = new UserDao();
+        UserDao userDao = new UserDaoFactory().userDao();
 //        userDao.add();
         User user = userDao.findById("2");
         System.out.println(user.getName());
---
+++ src/main/java/com/db/dao/UserDaoFactory.java
@@ -0,0 +1,10 @@
+package com.db.dao;
+
+public class UserDaoFactory {
+    public UserDao userDao(){
+        ConnectionMaker connectionMaker = new AwsConnectionMaker();
+        UserDao userDao = new UserDao(connectionMaker);
+        return userDao;
+
+    }
+}
